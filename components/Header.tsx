2a898ac85270fdca29c701b4c4dae4a0469ad12d
Edun Joshua Olubori <joshuaolubori@gmail.com>
2024-09-30 17:24:03 +0000

    minor typing fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@
 'use client'
 import { useState, useEffect } from "react"
 import Link from "next/link"
-import { usePathname } from 'next/navigation'
+// import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import { Menu, Coins, Leaf, Search, Bell, User, ChevronDown, LogIn } from "lucide-react"
 import { 
